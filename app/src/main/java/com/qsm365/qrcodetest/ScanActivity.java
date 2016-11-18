22a2b6b3ca70bb192313238322c36e532939d777
qsm365 <qsm365@qq.com>
2016-11-18 14:47:15 +0800

    修复了在android 6.0以上摄像头需要二次授权的兼容问题

--- app/src/main/java/com/qsm365/qrcodetest/ScanActivity.java
+++ app/src/main/java/com/qsm365/qrcodetest/ScanActivity.java
@@ -1,7 +1,11 @@
 package com.qsm365.qrcodetest;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -18,18 +22,66 @@ public class ScanActivity extends Activity implements ZXingScannerView.ResultHan
     private ZXingScannerView mScannerView;
     private String TAG = "MainActivity";
 
+    public static final int PERMISSION_REQUEST_CAMERA = 1;
+
+    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mScannerView = new ZXingScannerView(this);   // Programmatically initialize the scanner view
         setContentView(mScannerView);                // Set the scanner view as the content view
+
+        if (!haveCameraPermission())
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
     }
+
+    private boolean haveCameraPermission()
+    {
+        if (Build.VERSION.SDK_INT < 23)
+            return true;
+        return checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults)
+    {
+        // This is because the dialog was cancelled when we recreated the activity.
+        if (permissions.length == 0 || grantResults.length == 0)
+            return;
+
+        switch (requestCode)
+        {
+            case PERMISSION_REQUEST_CAMERA:
+            {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    startCamera();
+                }
+                else
+                {
+                    finish();
+                }
+            }
+            break;
+        }
+    }
+
+    public void startCamera()
+    {
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.startCamera();          // Start camera on resume
+    }
+
+    public void stopCamera()
+    {
+        mScannerView.stopCamera();
+    }
+
 
     @Override
     public void onResume() {
         super.onResume();
-        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
-        mScannerView.startCamera();          // Start camera on resume
+        startCamera();
         mScannerView.setAutoFocus(true);
         mScannerView.setKeepScreenOn(true);
     }
@@ -37,7 +89,7 @@ public class ScanActivity extends Activity implements ZXingScannerView.ResultHan
     @Override
     public void onPause() {
         super.onPause();
-        mScannerView.stopCamera();           // Stop camera on pause
+        stopCamera();
     }
 
     @Override
